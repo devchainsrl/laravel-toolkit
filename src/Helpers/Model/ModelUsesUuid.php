53a069ddb45e88d23eccc0cd614524b5ea2f6488
Gabi DJ (git) <git@gabisuciu.ro>
2021-05-13 18:20:01 +0300

    model uses uuid

---
+++ src/Helpers/Model/ModelUsesUuid.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Str;
+
+/**
+ * User: gabidj
+ * Date: 13.05.2021
+ * Time: 18:16
+ */
+
+trait ModelUsesUuid
+{
+    protected static function bootModelUsesUuid()
+    {
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string)Str::uuid();
+            }
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
